4a0e199e53b8f68edc8b7543ab596cb76a4c60b5
Kate Ramsey <kateramsey@live.com>
2016-04-20 15:25:07 -0500

    Change name of user object, add Tweat object

--- Tweater/Models/IdentityModels.cs
+++ Tweater/Models/IdentityModels.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -7,18 +10,33 @@ using Microsoft.AspNet.Identity.EntityFramework;
 namespace Tweater.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class TweaterUser : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<TweaterUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
         }
+
+        public string Username { get; set; }
+        public ICollection<TweaterUser> Following { get; set;}
+        public ICollection<TweaterUser> Followers { get; set; }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+
+    public class Tweat
+    {
+        public int Id { get; set; }
+        public TweaterUser Author { get; set; }
+        [StringLength(140, ErrorMessage = "The Tweat must be at most 140 characters long.")]
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<TweaterUser>
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
